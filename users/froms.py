ec232fc5341af3d50df3fc3659752389cbedd20f
van <ironvanxv@gmail.com>
2017-03-16 20:47:46 +0800

    login.regster-ready

--- users/froms.py
+++ users/froms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, min_length=5)
     password = forms.CharField(required=True, min_length=5)
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(required=True, min_length=5)
+    password = forms.CharField(required=True, min_length=5)
+    password2 = forms.CharField(required=True, min_length=5)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from .models import UserProfile
 from django.views.generic.base import View #View是一个get和post的一个系统，可以直接def post和get，
-from .froms import LoginForm
+from .froms import LoginForm, RegisterForm #载入form表单
+from django.core.mail import
+
+
 # Create your views here.
 
 
@@ -21,6 +25,22 @@ class CustomBackend(ModelBackend):
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html')
+    def post(self, request):
+        register_Form = RegisterForm(request.POST)
+        if register_Form.is_valid():
+            user_name = request.POST.get("username","")
+            pass_word = request.POST.get("password","")
+            pass_word2 = request.POST.get("password2","")
+            if pass_word != pass_word2:
+                return render(request, 'register.html', {"msg":"密码两次输入不一致"})
+            else:
+                user_profile = UserProfile()
+                user_profile.username = user_name
+                user_profile.email = user_name
+                user_profile.password = make_password(pass_word)
+                user_profile.save()
+        pass
+
 
 #下面是用View实现的get和post，效率很高。不用写if post和if get，用类解决这个问题。
 class LoginView(View):
@@ -39,7 +59,8 @@ class LoginView(View):
                 return render(request, "login.html", {"msg":"账号密码错误"})
         else:
             return render(request, "login.html", {"login_form": login_form})
-#
+
+
 # def user_login(request):
 #     if request.method == 'POST':
 #         user_name = request.POST.get("username", "")
