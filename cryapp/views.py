c27e9cd2455ad11b111b3ca0e5d3bf29015a5246
vanxv <ironvanxv@gmail.com>
2017-07-03 18:23:39 +0800

    DEBUG13

--- cryapp/views.py
+++ cryapp/views.py
@@ -200,7 +200,7 @@ def GetGoods(request, goodid):
                     printtext.write(str(datetime.now()) + str(len(phoneid_post)) + '188' + 'phoneid_post:' + str(phoneid_post))
                     printtext.close()
                     mobileidget = mobileid.objects.get(mobileid=phoneid_post)
-                    mobilelogcreate = mobilelog.objects.create(user=request.user,resip=ip(request), phoneid=mobileidget)
+                    mobilelogcreate = mobilelog.objects.create(user=request.user,resip=ip(request), phoneid=mobileidget.id)
                     mobilelogcreate.save()
 
 
