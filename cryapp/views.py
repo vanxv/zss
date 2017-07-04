68a45cf36c716bf97f3d79ae002735d77ac2b719
vanxv <ironvanxv@gmail.com>
2017-07-04 16:32:36 +0800

    select status =1

--- cryapp/views.py
+++ cryapp/views.py
@@ -151,15 +151,13 @@ class buyerIndex(View):
             select_cryorder = CryOrder.objects.filter()
             lockOrderlist = lockOrderAuthentication(request)
             buyerIndexSqlMove = ''
-            buyerIndexSql = 'SELECT * FROM cryapp_cryorder GROUP BY GoodId_id'
+            buyerIndexSql = 'SELECT Status = 1 FROM cryapp_cryorder GROUP BY GoodId_id'
             if len(lockOrderlist) == 1:
-                buyerIndexSqltest = 'SELECT * FROM cryapp_cryorder WHERE ShopId_id <> '+ lockOrderlist[0].id + ' GROUP BY GoodId_id'
-                buyerIndexSqltest1 = 'SELECT * FROM cryapp_cryorder WHERE ShopId_id <> ' + ' GROUP BY GoodId_id' + lockOrderlist[0].id
-                buyerIndexSql = 'SELECT * FROM cryapp_cryorder WHERE ShopId_id <> '+ lockOrderlist[0].id + ' GROUP BY GoodId_id'
+                buyerIndexSql = 'SELECT Status = 1 FROM cryapp_cryorder WHERE ShopId_id <> '+ lockOrderlist[0].id + ' GROUP BY GoodId_id'
             elif len(lockOrderlist) > 1:
                 for i in range(1, len(lockOrderlist)):
                     buyerIndexSqlMove = buyerIndexSqlMove + ' AND ShopId_id <>' + str(lockOrderlist[i].id)
-                buyerIndexSql = 'SELECT * FROM cryapp_cryorder WHERE ShopId_id <> '+ str(lockOrderlist[0].id) + buyerIndexSqlMove + ' ' + ' GROUP BY GoodId_id'
+                buyerIndexSql = 'SELECT Status = 1 FROM cryapp_cryorder WHERE ShopId_id <> '+ str(lockOrderlist[0].id) + buyerIndexSqlMove + ' ' + ' GROUP BY GoodId_id'
             orderdict = CryOrder.objects.raw(buyerIndexSql)
             # Html_file= open("bug.html","w")
             # Html_file.write(datetime.now()+buyerIndexSql+type(orderdict)+orderdict)
