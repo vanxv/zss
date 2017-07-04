e1095813b7770cd33422085cf5e2730c34cb6c41
vanxv <ironvanxv@gmail.com>
2017-07-04 15:56:12 +0800

    debug36

--- cryapp/views.py
+++ cryapp/views.py
@@ -160,7 +160,7 @@ class buyerIndex(View):
                 buyerIndexSql = 'SELECT * FROM cryapp_cryorder WHERE ShopId_id <> '+ str(lockOrderlist[0].id) + buyerIndexSqlMove + ' GROUP BY GoodId_id'
             orderdict = CryOrder.objects.raw(buyerIndexSql)
             Html_file= open("bug.html","w")
-            Html_file.write(buyerIndexSql)
+            Html_file.write(datetime.now()+buyerIndexSql+type(orderdict)+orderdict)
             Html_file.close
             return render(request, 'material/index.html', {'orderdict':orderdict})
         else:
