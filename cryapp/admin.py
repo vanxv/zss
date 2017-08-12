b49f6ee2918878b07058b63099cb09e5835258ff
vanxv <ironvanxv@gmail.com>
2017-08-12 18:04:35 +0800

    admin views

--- cryapp/admin.py
+++ cryapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import CryOrder
 
-admin.site.register(CryOrder)
+class CryOrderAdmin(admin.ModelAdmin):
+    list_display = ('id','Keywords','platform','Status','AddTime','OrderSort','GoodId_id','ShopId_id','Userid_id','buyerid_id','Money')
+    list_filter = ('id','Keywords','platform','Status','AddTime','OrderSort','GoodId_id','ShopId_id','Userid_id','buyerid_id','Money')
+admin.site.register(CryOrder, CryOrderAdmin)
 # Register your models here.
